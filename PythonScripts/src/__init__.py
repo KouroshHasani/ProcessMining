1bba1bc747cf574e875fdda062f87f4e92eae419
KouroshHasani <KouroshHasani@github.com>
2024-02-11 14:54:16 +0330

    change python src dir

--- PythonScripts/src/__init__.py
+++ PythonScripts/src/__init__.py
@@ -1,3 +1,3 @@
 from pathlib import Path
-DATA_DIR = Path(__file__).resolve().parent / 'Datasets'
-IMAGE_DIR = Path(__file__).resolve().parent / 'Images'
+
+SRC_DIR = Path(__file__).resolve().parent
